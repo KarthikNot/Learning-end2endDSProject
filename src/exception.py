7ed4bea98c728440cc58a13036c9cc1a94cba1f7
KarthikNot <anumallakarthikeyan03@gmail.com>
2025-07-08 00:13:21 +0530

    resolved errors

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error , error_detail : sys):
     
 class CustomException(Exception):
     def __init__(self, error, error_detail : sys):
-        super.__init__(error)
+        super().__init__(error)
         self.error = error_message_detail(error=error, error_detail=error_detail)
                  
     def __str__(self):
